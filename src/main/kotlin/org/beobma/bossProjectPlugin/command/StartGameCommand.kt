d79046b280909928d31093b75772da7ea891fafa
BEOBMA <dingxiao19@gmail.com>
2026-04-22 21:52:19 +0900

    Replace ChatColor messages with MiniMessage

--- src/main/kotlin/org/beobma/bossProjectPlugin/command/StartGameCommand.kt
+++ src/main/kotlin/org/beobma/bossProjectPlugin/command/StartGameCommand.kt
@@ -2,7 +2,7 @@ package org.beobma.bossProjectPlugin.command
 
 import org.beobma.bossProjectPlugin.manager.GameManager
 import org.beobma.bossProjectPlugin.manager.GameStartInvitationManager
-import org.bukkit.ChatColor
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player
 class StartGameCommand(
     private val invitationManager: GameStartInvitationManager
 ) : CommandExecutor {
+    private val miniMessage = MiniMessage.miniMessage()
 
     override fun onCommand(
         sender: CommandSender,
@@ -19,22 +20,22 @@ class StartGameCommand(
         args: Array<out String>
     ): Boolean {
         val player = sender as? Player ?: run {
-            sender.sendMessage("플레이어만 사용할 수 있는 명령어입니다.")
+            sender.sendMessage(miniMessage.deserialize("<red>플레이어만 사용할 수 있는 명령어입니다.</red>"))
             return true
         }
 
         if (!player.isOp) {
-            player.sendMessage("${ChatColor.RED}OP 권한이 있어야 게임을 시작할 수 있습니다.")
+            player.sendMessage(miniMessage.deserialize("<red>OP 권한이 있어야 게임을 시작할 수 있습니다.</red>"))
             return true
         }
 
         if (GameManager.getCurrentGame() != null) {
-            player.sendMessage("${ChatColor.RED}이미 진행 중인 게임이 있습니다.")
+            player.sendMessage(miniMessage.deserialize("<red>이미 진행 중인 게임이 있습니다.</red>"))
             return true
         }
 
         if (invitationManager.hasActiveSession()) {
-            player.sendMessage("${ChatColor.RED}이미 게임 시작 투표가 진행 중입니다.")
+            player.sendMessage(miniMessage.deserialize("<red>이미 게임 시작 투표가 진행 중입니다.</red>"))
             return true
         }
 
