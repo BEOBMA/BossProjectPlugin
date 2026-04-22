d91c8ab523af862811abd56c23fc3c071a7bf25b
BEOBMA <ssdssdtkfk@naver.com>
2026-04-22 21:45:45 +0900

    feat: 수정

--- src/main/kotlin/org/beobma/bossProjectPlugin/job/registry/JobRegistry.kt
+++ src/main/kotlin/org/beobma/bossProjectPlugin/job/registry/JobRegistry.kt
@@ -1,18 +1,11 @@
 package org.beobma.bossProjectPlugin.job.registry
 
 import org.beobma.bossProjectPlugin.job.Job
-import org.beobma.bossProjectPlugin.job.list.ArcherJob
-import org.beobma.bossProjectPlugin.job.list.GuardianJob
-import org.beobma.bossProjectPlugin.job.list.MageJob
-import org.beobma.bossProjectPlugin.job.list.WarriorJob
 import kotlin.reflect.KClass
 
 object JobRegistry {
     private val jobs: List<KClass<out Job>> = listOf(
-        WarriorJob::class,
-        ArcherJob::class,
-        MageJob::class,
-        GuardianJob::class
+
     )
 
     fun all(): List<KClass<out Job>> = jobs
