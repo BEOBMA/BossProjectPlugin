8500ccdf6ff022c4bfb5b2971848912b6ca8ebdc
BEOBMA <dingxiao19@gmail.com>
2026-04-24 18:09:32 +0900

    보스바 표시 및 보스 인터랙션 피해 연동

--- src/main/kotlin/org/beobma/bossProjectPlugin/entity/enemy/EnemyData.kt
+++ src/main/kotlin/org/beobma/bossProjectPlugin/entity/enemy/EnemyData.kt
@@ -7,6 +7,8 @@ import org.beobma.bossProjectPlugin.entity.enemy.skill.PatternSkill
 abstract class EnemyData : EntityData() {
     abstract val maxHealth: Double
     abstract var health: Double
+    open val displayName: String
+        get() = this::class.simpleName ?: "Unknown Boss"
 
     abstract val passives: List<BossPassive>
     abstract val patternSkills: List<PatternSkill>
--- src/main/kotlin/org/beobma/bossProjectPlugin/manager/BossInteractionDamageListener.kt
+++ src/main/kotlin/org/beobma/bossProjectPlugin/manager/BossInteractionDamageListener.kt
@@ -2,29 +2,21 @@ package org.beobma.bossProjectPlugin.manager
 
 import org.beobma.bossProjectPlugin.entity.enemy.BossCombatConstants
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
-import org.bukkit.event.entity.EntityDamageEvent
 
 object BossInteractionDamageListener : Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onBossInteractionDamaged(event: EntityDamageByEntityEvent) {
         if (!event.entity.scoreboardTags.contains(BossCombatConstants.BOSS_INTERACTION_TAG)) {
             return
         }
 
-        val finalDamage = event.finalDamage
-        val cause = event.cause
-
-        event.entity.customName = "boss_interaction: ${"%.2f".format(finalDamage)} (${cause.name})"
-    }
-
-    @EventHandler
-    fun onGenericBossInteractionDamage(event: EntityDamageEvent) {
-        if (!event.entity.scoreboardTags.contains(BossCombatConstants.BOSS_INTERACTION_TAG)) {
-            return
-        }
-
-        event.entity.customName = "boss_interaction: ${"%.2f".format(event.finalDamage)}"
+        GameManager.applyBossInteractionDamage(
+            attacker = event.damager,
+            damaged = event.entity,
+            finalDamage = event.finalDamage
+        )
     }
 }
